89bbc921c04c1044dc20b2253ca33f100af64477
Raj Patel <raj17ce@gmail.com>
2024-03-28 11:20:03 +0530

    Modify .gitignore for logs, used ASSET_TRUE, ASSERT_FALSE in ValidateTest.cpp

--- Employee Database Test/src/ValidateTest.cpp
+++ Employee Database Test/src/ValidateTest.cpp
@@ -4,111 +4,111 @@
 using EmployeeDB::Validate;
 
 TEST(TestValidateEmail, ValidateEmailTrue) {
-	ASSERT_EQ(Validate::validateEmail("hello@gmail.com"), true);
-	ASSERT_EQ(Validate::validateEmail("hello@gmail.co.uk"), true);
-	ASSERT_EQ(Validate::validateEmail("hello.hii@gmail.com"), true);
-	ASSERT_EQ(Validate::validateEmail("hello123@gmail.shop.in"), true);
-	ASSERT_EQ(Validate::validateEmail("123hello@gmail.com"), true);
+	ASSERT_TRUE(Validate::validateEmail("hello@gmail.com"));
+	ASSERT_TRUE(Validate::validateEmail("hello@gmail.co.uk"));
+	ASSERT_TRUE(Validate::validateEmail("hello.hii@gmail.com"));
+	ASSERT_TRUE(Validate::validateEmail("hello123@gmail.shop.in"));
+	ASSERT_TRUE(Validate::validateEmail("123hello@gmail.com"));
 }
 
 TEST(TestValidateEmail, ValidateEmailFalse) {
-	ASSERT_EQ(Validate::validateEmail("hello@@gmail.com"), false);
-	ASSERT_EQ(Validate::validateEmail("hello@gmai$l.couk"), false);
-	ASSERT_EQ(Validate::validateEmail("hello-hiii@gmail.com"), false);
-	ASSERT_EQ(Validate::validateEmail("hello123$@gmail.shop.in"), false);
-	ASSERT_EQ(Validate::validateEmail("123-alan@gmail.co.in.kom"), false);
+	ASSERT_FALSE(Validate::validateEmail("hello@@gmail.com"));
+	ASSERT_FALSE(Validate::validateEmail("hello@gmai$l.couk"));
+	ASSERT_FALSE(Validate::validateEmail("hello-hiii@gmail.com"));
+	ASSERT_FALSE(Validate::validateEmail("hello123$@gmail.shop.in"));
+	ASSERT_FALSE(Validate::validateEmail("123-alan@gmail.co.in.kom"));
 }
 
 TEST(TestValidateMobile, ValidateMobileTrue) {
-	ASSERT_EQ(Validate::validateMobile("9875445568"), true);
-	ASSERT_EQ(Validate::validateMobile("9521364578"), true);
-	ASSERT_EQ(Validate::validateMobile("6548752369"), true);
-	ASSERT_EQ(Validate::validateMobile("7230154030"), true);
-	ASSERT_EQ(Validate::validateMobile("8080406085"), true);
+	ASSERT_TRUE(Validate::validateMobile("9875445568"));
+	ASSERT_TRUE(Validate::validateMobile("9521364578"));
+	ASSERT_TRUE(Validate::validateMobile("6548752369"));
+	ASSERT_TRUE(Validate::validateMobile("7230154030"));
+	ASSERT_TRUE(Validate::validateMobile("8080406085"));
 }
 
 TEST(TestValidateMobile, ValidateMobileFalse) {
-	ASSERT_EQ(Validate::validateMobile("1234567989"), false);
-	ASSERT_EQ(Validate::validateMobile("9876"), false);
-	ASSERT_EQ(Validate::validateMobile("3920254252"), false);
-	ASSERT_EQ(Validate::validateMobile("8530157511111"), false);
-	ASSERT_EQ(Validate::validateMobile("2635458545"), false);
+	ASSERT_FALSE(Validate::validateMobile("1234567989"));
+	ASSERT_FALSE(Validate::validateMobile("9876"));
+	ASSERT_FALSE(Validate::validateMobile("3920254252"));
+	ASSERT_FALSE(Validate::validateMobile("8530157511111"));
+	ASSERT_FALSE(Validate::validateMobile("2635458545"));
 }
 
 TEST(TestValidateDate, ValidateDateTrue) {
-	ASSERT_EQ(Validate::validateDate("17/04/2003"), true);
-	ASSERT_EQ(Validate::validateDate("17-04-2003"), true);
-	ASSERT_EQ(Validate::validateDate("17.04.2003"), true);
-	ASSERT_EQ(Validate::validateDate("29/02/2024"), true);
-	ASSERT_EQ(Validate::validateDate("31/12/2024"), true);
+	ASSERT_TRUE(Validate::validateDate("17/04/2003"));
+	ASSERT_TRUE(Validate::validateDate("17-04-2003"));
+	ASSERT_TRUE(Validate::validateDate("17.04.2003"));
+	ASSERT_TRUE(Validate::validateDate("29/02/2024"));
+	ASSERT_TRUE(Validate::validateDate("31/12/2024"));
 }
 
 TEST(TestValidateDate, ValidateDateFalse) {
-	ASSERT_EQ(Validate::validateDate("17/04-2003"), false);
-	ASSERT_EQ(Validate::validateDate("17.13.2003"), false);
-	ASSERT_EQ(Validate::validateDate("31/04/2003"), false);
-	ASSERT_EQ(Validate::validateDate("29/02/2023"), false);
-	ASSERT_EQ(Validate::validateDate("32/12/2024"), false);
+	ASSERT_FALSE(Validate::validateDate("17/04-2003"));
+	ASSERT_FALSE(Validate::validateDate("17.13.2003"));
+	ASSERT_FALSE(Validate::validateDate("31/04/2003"));
+	ASSERT_FALSE(Validate::validateDate("29/02/2023"));
+	ASSERT_FALSE(Validate::validateDate("32/12/2024"));
 }
 
 TEST(TestValidateGender, ValidateGenderTrue) {
-	ASSERT_EQ(Validate::validateGender(std::string{ "Male" }), true);
-	ASSERT_EQ(Validate::validateGender(std::string{ "Female" }), true);
-	ASSERT_EQ(Validate::validateGender(std::string{ "Other" }), true);
-	ASSERT_EQ(Validate::validateGender(std::string{ "mAlE" }), true);
-	ASSERT_EQ(Validate::validateGender(std::string{ "FeMaLe" }), true);
+	ASSERT_TRUE(Validate::validateGender(std::string{ "Male" }));
+	ASSERT_TRUE(Validate::validateGender(std::string{ "Female" }));
+	ASSERT_TRUE(Validate::validateGender(std::string{ "Other" }));
+	ASSERT_TRUE(Validate::validateGender(std::string{ "mAlE" }));
+	ASSERT_TRUE(Validate::validateGender(std::string{ "FeMaLe" }));
 }
 
 TEST(TestValidateGender, ValidateGenderFalse) {
-	ASSERT_EQ(Validate::validateGender(std::string{ "Malee" }), false);
-	ASSERT_EQ(Validate::validateGender(std::string{ "females" }), false);
-	ASSERT_EQ(Validate::validateGender(std::string{ "Otherl" }), false);
-	ASSERT_EQ(Validate::validateGender(std::string{ "    " }), false);
-	ASSERT_EQ(Validate::validateGender(std::string{ "femalles" }), false);
+	ASSERT_FALSE(Validate::validateGender(std::string{ "Malee" }));
+	ASSERT_FALSE(Validate::validateGender(std::string{ "females" }));
+	ASSERT_FALSE(Validate::validateGender(std::string{ "Otherl" }));
+	ASSERT_FALSE(Validate::validateGender(std::string{ "    " }));
+	ASSERT_FALSE(Validate::validateGender(std::string{ "femalles" }));
 }
 
 TEST(TestValidateEmployeeID, ValidateEmployeeIDTrue) {
-	ASSERT_EQ(Validate::validateEmployeeID("1","Engineer"), true);
-	ASSERT_EQ(Validate::validateEmployeeID("8","Finance"), true);
-	ASSERT_EQ(Validate::validateEmployeeID("15","HR"), true);
-	ASSERT_EQ(Validate::validateEmployeeID("22","QA"), true);
-	ASSERT_EQ(Validate::validateEmployeeID("5","Engineer"), true);
+	ASSERT_TRUE(Validate::validateEmployeeID("1","Engineer"));
+	ASSERT_TRUE(Validate::validateEmployeeID("8","Finance"));
+	ASSERT_TRUE(Validate::validateEmployeeID("15","HR"));
+	ASSERT_TRUE(Validate::validateEmployeeID("22","QA"));
+	ASSERT_TRUE(Validate::validateEmployeeID("5","Engineer"));
 }
 
 TEST(TestValidateEmployeeID, ValidateEmployeeIDFalse) {
-	ASSERT_EQ(Validate::validateEmployeeID("1", "Finance"), false);
-	ASSERT_EQ(Validate::validateEmployeeID("8", "Engineer"), false);
-	ASSERT_EQ(Validate::validateEmployeeID("150", "HR"), false);
-	ASSERT_EQ(Validate::validateEmployeeID("20", "QA"), false);
-	ASSERT_EQ(Validate::validateEmployeeID("50", "Engineer"), false);
+	ASSERT_FALSE(Validate::validateEmployeeID("1", "Finance"));
+	ASSERT_FALSE(Validate::validateEmployeeID("8", "Engineer"));
+	ASSERT_FALSE(Validate::validateEmployeeID("150", "HR"));
+	ASSERT_FALSE(Validate::validateEmployeeID("20", "QA"));
+	ASSERT_FALSE(Validate::validateEmployeeID("50", "Engineer"));
 }
 
 TEST(TestValidateManagerID, ValidateManagerIDTrue) {
-	ASSERT_EQ(Validate::validateManagerID("1"), true);
-	ASSERT_EQ(Validate::validateManagerID("8"), true);
-	ASSERT_EQ(Validate::validateManagerID("15"), true);
-	ASSERT_EQ(Validate::validateManagerID("23"), true);
-	ASSERT_EQ(Validate::validateManagerID("5"), true);
+	ASSERT_TRUE(Validate::validateManagerID("1"));
+	ASSERT_TRUE(Validate::validateManagerID("8"));
+	ASSERT_TRUE(Validate::validateManagerID("15"));
+	ASSERT_TRUE(Validate::validateManagerID("23"));
+	ASSERT_TRUE(Validate::validateManagerID("5"));
 }
 
 TEST(TestValidateManagerID, ValidateManagerIDFalse) {
-	ASSERT_EQ(Validate::validateManagerID("10"), false);
-	ASSERT_EQ(Validate::validateManagerID("800"), false);
-	ASSERT_EQ(Validate::validateManagerID("-5"), false);
-	ASSERT_EQ(Validate::validateManagerID("Hello"), false);
-	ASSERT_EQ(Validate::validateManagerID("25"), false);
+	ASSERT_FALSE(Validate::validateManagerID("10"));
+	ASSERT_FALSE(Validate::validateManagerID("800"));
+	ASSERT_FALSE(Validate::validateManagerID("-5"));
+	ASSERT_FALSE(Validate::validateManagerID("Hello"));
+	ASSERT_FALSE(Validate::validateManagerID("25"));
 }
 
 TEST(TestValidateDepartmentID, ValidateDepartmentIDTrue) {
-	ASSERT_EQ(Validate::validateDepartmentID("1"), true);
-	ASSERT_EQ(Validate::validateDepartmentID("2"), true);
-	ASSERT_EQ(Validate::validateDepartmentID("3"), true);
-	ASSERT_EQ(Validate::validateDepartmentID("4"), true);
+	ASSERT_TRUE(Validate::validateDepartmentID("1"));
+	ASSERT_TRUE(Validate::validateDepartmentID("2"));
+	ASSERT_TRUE(Validate::validateDepartmentID("3"));
+	ASSERT_TRUE(Validate::validateDepartmentID("4"));
 }
 
 TEST(TestValidateDepartmentID, ValidateDepartmentIDFalse) {
-	ASSERT_EQ(Validate::validateDepartmentID("-1"), false);
-	ASSERT_EQ(Validate::validateDepartmentID("0"), false);
-	ASSERT_EQ(Validate::validateDepartmentID("300"), false);
-	ASSERT_EQ(Validate::validateDepartmentID("4Hello"), false);
+	ASSERT_FALSE(Validate::validateDepartmentID("-1"));
+	ASSERT_FALSE(Validate::validateDepartmentID("0"));
+	ASSERT_FALSE(Validate::validateDepartmentID("300"));
+	ASSERT_FALSE(Validate::validateDepartmentID("4Hello"));
 }
